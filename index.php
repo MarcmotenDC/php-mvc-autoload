905a43fd700ac7719d6878b6476cc31c569f49d1
MarcmotenDC <superman868789@gmail.com>
2024-03-27 18:47:22 -0500

    fix url for server

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
 // ini_set('display_startup_errors', '1');
 // error_reporting(E_ALL);
 // Server
-// $path = str_replace("/webdev/SteveHarvey/php/mvc-routing/", "/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+// !Change URL for every site!
+$path = str_replace("/webdev/SteveHarvey/mvc-autoload/", "/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 // Local
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 
 spl_autoload_register(function (string $class_name) {
